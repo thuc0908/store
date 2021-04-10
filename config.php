b67dbdef763adccaa2fb506969017aca0433f9ef
thuc0908 <82256869+thuc0908@users.noreply.github.com>
2021-04-10 15:10:06 +0700

    store

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+$hostname     = "localhost";
+$username     = "qua59_admin";
+$password     = "quangvinh";
+$databasename = "qua59b77_amnhac";
+$visitorTimeout = 900; // =15 * 60 Download source mien phi tai Sharecode.vn
+
+$MAXPAGE = 10;
+$multiLanguage = 1;//0 : single  ;  1 : multi
+
+$arrLanguage = array(
+	array('vn','Việt Nam'),
+	array('en','English')
+
+);
+
+?>
+
